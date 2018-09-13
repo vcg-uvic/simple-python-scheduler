0c4f0e5d06d26c1597dc83c7d45d878a311ac442
Kwang Moo Yi <kwang.m.yi@gmail.com>
2018-09-13 14:52:31 -0700

    typo fix

--- sps/srunsched.py
+++ sps/srunsched.py
@@ -263,7 +263,7 @@ def get_free_gpus():
                 dir_cur_gpu, job))
             assigned = True
         if not assigned:
-            free_gpu += [int(dir_cur_gpu.split["/"][-1])]
+            free_gpus += [int(dir_cur_gpu.split["/"][-1])]
 
     return free_gpus
 
